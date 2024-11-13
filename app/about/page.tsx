10fe823bb35d606969d154d9d0454cbb1ab5bb62
arsahak <arsahak99@gmail.com>
2024-11-13 17:19:02 +0600

    udpate overflow contact page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 
 const page = () => {
   return (
-    <div>
+    <div className="overflow-y-hidden">
       <ScrollMotionEffect>
         <AboutSection />
         <StatsSection />
