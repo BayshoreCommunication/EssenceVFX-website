c6785f8caa4f0c42cb0f2f2fcafdfac52c9391d7
arsahak <arsahak99@gmail.com>
2024-12-01 17:56:49 +0600

    update

--- components/gallery/GalleryPage.tsx
+++ components/gallery/GalleryPage.tsx
@@ -130,7 +130,7 @@ const GalleryPage = () => {
                         width={500}
                         height={500}
                         src={el?.url}
-                        alt={"Gallery Images"}
+                        alt={"Gallery Image"}
                       />
                     </div>
                   </SwiperSlide>
