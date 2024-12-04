7fc24b9797980a8160bba751d21e162ea92883cf
arsahak <arsahak99@gmail.com>
2024-12-04 16:58:57 +0600

    update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,14 +1,14 @@
 import AboutSection from "@/components/about/AboutSection";
 import StatsSection from "@/components/about/StatsSection";
 
-import React from "react";
-
 export const metadata = {
   title: "EssenceVFX",
   description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi imperdiet, mauris vitae molestie gravida, libero lorem fermentum elit, eu placerat nunc elit id massa. Morbi interdum lectus ut mauris vehicula",
+  openGraph: {
+    images: "/opengraph-image.jpg",
+  },
 };
-
 const page = () => {
   return (
     <div className="overflow-y-hidden">
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,11 +1,13 @@
 import ContactSection from "@/components/contact/ContactSection";
 import ScrollMotionEffect from "@/components/motion/ScrollMotionEffect";
-import React from "react";
 
 export const metadata = {
   title: "EssenceVFX",
   description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi imperdiet, mauris vitae molestie gravida, libero lorem fermentum elit, eu placerat nunc elit id massa. Morbi interdum lectus ut mauris vehicula",
+  openGraph: {
+    images: "/opengraph-image.jpg",
+  },
 };
 
 const page = () => {
