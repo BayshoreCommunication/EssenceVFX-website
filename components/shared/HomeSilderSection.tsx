70fb7b2fd38a386c13d309b08fe92a645960679a
arsahak <arsahak99@gmail.com>
2024-12-12 12:47:59 +0600

    fix image issue

--- components/shared/HomeSilderSection.tsx
+++ components/shared/HomeSilderSection.tsx
@@ -221,7 +221,7 @@ const HomeSilderSection = () => {
               {sliderPortfolioData?.map((el, index) => (
                 <SwiperSlide key={index} className="">
                   <div className="flex items-center justify-center">
-                    <button
+                    <div
                       className="relative w-[400px] h-[562px] cursor-pointer  group mt-8"
                       onClick={() => {
                         onOpen();
@@ -250,7 +250,7 @@ const HomeSilderSection = () => {
                         src={"/assets/home/image-hover-border-new.png"}
                         width={500}
                       />
-                    </button>
+                    </div>
                   </div>
                 </SwiperSlide>
               ))}
