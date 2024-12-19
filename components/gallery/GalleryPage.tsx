a39f3ee35da29307c206c9df53029127dfbfc819
Faisal Ahamed Rahad <155021870+rahad-dev24@users.noreply.github.com>
2024-12-19 14:40:18 +0600

    testing chad

--- components/gallery/GalleryPage.tsx
+++ components/gallery/GalleryPage.tsx
@@ -3,7 +3,7 @@ import { useAppContext } from "@/app/AppContext";
 import { gallerySilderData } from "@/config/data";
 import { useDisclosure } from "@nextui-org/react";
 import Image from "next/image";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { use, useCallback, useEffect, useMemo, useRef, useState } from "react";
 import {
   Autoplay,
   Keyboard,
@@ -18,9 +18,20 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import ScrollMotionEffect from "../motion/ScrollMotionEffect";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "../ui/carousel";
+import { Card, CardContent } from "../ui/card";
 
 const GalleryPage = () => {
-  const memoizedGalleryData = useMemo(() => gallerySilderData, []);
+  const memoizedGalleryData = gallerySilderData;
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  // const memoizedGalleryData = useMemo(() => gallerySilderData, []);
+  const [timer, setTimer] = useState(0);
 
   const { silderIndexValue, setSilderIndexValue } = useAppContext();
   const [videoUrl, setVideoUrl] = useState("");
@@ -38,7 +49,7 @@ const GalleryPage = () => {
       swiperRef.current.navigation.init();
       swiperRef.current.navigation.update();
     }
-  }, []);
+  }, [currentImageIndex]);
 
   // Sync Swiper with `silderIndexValue` smoothly
   useEffect(() => {
@@ -55,25 +66,52 @@ const GalleryPage = () => {
     setImageUrl(image);
   }, []);
 
-  const handleSlideChange = useCallback(
-    (swiper: any) => {
-      if (swiper.activeIndex >= memoizedGalleryData.length) {
-        swiper.slideTo(0); // Reset to the first slide
-        setSilderIndexValue(0);
-      } else {
-        setSilderIndexValue(swiper.activeIndex);
-      }
-    },
-    [memoizedGalleryData.length, setSilderIndexValue]
-  );
+  // const handleSlideChange = useCallback(
+  //   (swiper: any) => {
+  //     if (swiper.activeIndex >= memoizedGalleryData.length) {
+  //       swiper.slideTo(0); // Reset to the first slide
+  //       setSilderIndexValue(0);
+  //     } else {
+  //       setSilderIndexValue(swiper.activeIndex);
+  //     }
+  //   },
+  //   [memoizedGalleryData.length, setSilderIndexValue]
+  // );
 
-  const handlePrevSlide = useCallback(() => {
+  // const handlePrevSlide = useCallback(() => {
+  //   if (silderIndexValue > 0) {
+  //     setSilderIndexValue((prev: number) => prev - 1);
+  //   }
+  // }, [silderIndexValue]);
+
+  // const handleNextSlide = useCallback(() => {
+  //   if (silderIndexValue >= gallerySilderData.length - 1) {
+  //     setSilderIndexValue(0); // Reset to the first index
+  //     if (swiperRef.current) {
+  //       swiperRef.current.slideTo(0);
+  //     }
+  //   } else {
+  //     setSilderIndexValue((prev: number) => prev + 1);
+  //   }
+  // }, [silderIndexValue, memoizedGalleryData.length]);
+  const handleSlideChange = (swiper: any) => {
+    setCurrentImageIndex(swiper.activeIndex);
+
+    if (swiper.activeIndex >= memoizedGalleryData.length) {
+      swiper.slideTo(0); // Reset to the first slide
+      setSilderIndexValue(0);
+    } else {
+      setSilderIndexValue(swiper.activeIndex);
+    }
+  };
+
+  const handlePrevSlide = () => {
     if (silderIndexValue > 0) {
       setSilderIndexValue((prev: number) => prev - 1);
     }
-  }, [silderIndexValue]);
+  };
 
-  const handleNextSlide = useCallback(() => {
+  const handleNextSlide = () => {
     if (silderIndexValue >= gallerySilderData.length - 1) {
       setSilderIndexValue(0); // Reset to the first index
       if (swiperRef.current) {
@@ -82,7 +120,16 @@ const GalleryPage = () => {
     } else {
       setSilderIndexValue((prev: number) => prev + 1);
     }
-  }, [silderIndexValue, memoizedGalleryData.length]);
+  };
+  useEffect(() => {
+    const clickHandler = document.querySelector(".clickable") as HTMLElement;
+    if (clickHandler) {
+      setTimeout(() => {
+        clickHandler.click();
+        setTimer(timer + 1);
+      }, 2000);
+    }
+  }, [timer]);
 
   return (
     <div className="relative bg-white pt-28 lg:pt-16 pb-8 lg:pb-20">
@@ -91,7 +138,7 @@ const GalleryPage = () => {
           Gallery
         </h2>
 
-        <div className="container">
+        <div className="container hidden md:block">
           <div className="flex items-center justify-between">
             <div className="w-[0%] relative z-50 left-[0%] lg:-left-24 ">
               <button
@@ -131,7 +178,7 @@ const GalleryPage = () => {
                   },
                   768: {
                     slidesPerView: 1,
-                    spaceBetween: 30,
+                    spaceBetween: 20,
                   },
                   1024: {
                     slidesPerView: 3,
@@ -142,7 +189,7 @@ const GalleryPage = () => {
                 {memoizedGalleryData.map((el, index) => (
                   <SwiperSlide key={el.url || index}>
                     <div
-                      className="cursor-pointer w-[50%] h-auto m-auto"
+                      className="cursor-pointer"
                       onClick={() => {
                         // Resume autoplay when clicking on an image
                         if (swiperRef.current) {
@@ -151,11 +198,14 @@ const GalleryPage = () => {
                       }}
                     >
                       <Image
-                        className="w-[422px] h-[485px] transition-all duration-700 ease-in-out"
-                        width={200}
-                        height={200}
+                        // className="w- h-full
+                        //  transition-all duration-700 ease-in-out"
+                        className=" w-[422px] h-[435px] md:h-[485px] transition-all duration-700 ease-in-out"
+                        width={1000}
+                        height={1000}
                         src={el.url}
                         alt={`Gallery Image ${index + 1}`}
+                        priority
                         quality={100}
                       />
                     </div>
@@ -176,6 +226,25 @@ const GalleryPage = () => {
           </div>
         </div>
       </ScrollMotionEffect>
+      <Carousel opts={{ loop: true }} className="w-[84%] m-auto md:hidden">
+        <CarouselContent>
+          {memoizedGalleryData.map((el, index) => (
+            <CarouselItem key={index}>
+              <Image
+                className=""
+                width={1000}
+                height={1000}
+                src={el.url}
+                alt={`Gallery Image ${index + 1}`}
+                priority
+                quality={100}
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 };
